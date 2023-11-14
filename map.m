8b1a38ef5b1bb48b8c3264f13940a30ed40f08b0
StanleyWang1 <swang11@stanford.edu>
2023-11-14 00:18:45 -0800

    added more agents and stochastic movement

--- map.m
+++ map.m
@@ -7,7 +7,7 @@
 classdef map
     properties
         img % downsampled square image to show in background for aesthetics
-        dim = 20 % discretize state space to dim x dim rectilinear grid
+        dim = 50 % discretize state space to dim x dim rectilinear grid
         img_px = 1000 % rescale image to img_px x img_px pixels
         box_px % size of bounding box of one "state" in grid in image pixel units
     end
@@ -35,8 +35,38 @@ classdef map
             scaledYTickValues = yticks/obj.box_px; yticklabels(arrayfun(@num2str, scaledYTickValues, 'UniformOutput', false));
         end
         %% STORM PLOTTER
-        function draw_storm(obj, fig_num)
+        function draw_storm(obj, fig_num, c, r)
+            cx = c(1); cy = c(2); % center of storm circle
             figure(fig_num)
+            F = []; V = []; iters = 0; % initialize patch face/vertex data
+            % Generate patches
+            for xi = 1:obj.dim
+                for yi = 1:obj.dim
+                   if norm([xi-cx, yi-cy]) >= r % if in storm
+                       % falloff = 0.1;
+                       % intensity = min(1, falloff*(norm([xi-cx, yi-cy])-r));
+                       BLx = obj.box_px*(xi-1); 
+                       BLy = obj.box_px*(yi-1);
+                       F = [F; [1, 2, 3, 4]+4*iters];
+                       V = [V; [BLx, BLy; BLx+obj.box_px, BLy; BLx+obj.box_px, BLy+obj.box_px; BLx, BLy+obj.box_px]];
+                       iters = iters + 1;
+                   end
+                end
+            end
+            % Draw Patch only once for efficiency
+            patch('Faces', F, 'Vertices', V, 'FaceColor', [165/255 ,81/255, 227/255], 'FaceAlpha', 0.5);
+        end
+        %% AGENT PLOTTER
+        function draw_agent(obj, fig_num, x, y, color)
+            % Plot agent at x and y
+            figure(fig_num)
+            BLx = obj.box_px*(x-1); 
+            BLy = obj.box_px*(y-1);
+            sidelength = obj.box_px;
+            patch('Faces', [1, 2, 3, 4], 'Vertices', [BLx, BLy; ...
+                BLx+sidelength, BLy; BLx+sidelength, ...
+                BLy+sidelength; BLx, BLy+sidelength], ...
+                'FaceColor', color);
         end
     end
 end
--- test.m
+++ test.m
@@ -7,6 +7,47 @@
 imagepath = './assets/map.jpeg';
 
 M = map(imagepath);
-M.draw_map(1);
-% set(gcf, 'position', [ 1   676   208*3   165*3]);
+r = linspace(25, 5, 100);
 
+% Video creation
+vidfile = VideoWriter('demo_storm_closure', 'MPEG-4');
+vidfile.FrameRate = 30;
+vidfile.Quality = 100;
+open(vidfile);
+
+% Initialize agent position 
+A = [20 23
+    35 11
+    12 36
+    24 32
+    15 19
+    45 23
+    37 22];
+colors = {'red', 'green', 'blue', 'cyan', 'magenta', 'yellow', 'white'};
+% Simulate
+for i = 1:100
+    M.draw_map(1);
+    hold on;
+    for j = 1:7
+        R = randi([1, 5]);
+        if R == 1
+            A(j,1) = max(min(A(j,1) + 1, 50), 0);
+        elseif R == 2
+            A(j,1) = max(min(A(j,1) - 1, 50), 0);
+        elseif R == 3
+            A(j,2) = max(min(A(j,2) + 1, 50), 0);
+        elseif R == 4
+            A(j,2) = max(min(A(j,2) - 1, 50), 0);
+        end
+        M.draw_agent(1, A(j,1), A(j,2), colors{j});
+    end
+    M.draw_storm(1, [20,23], r(i));
+    hold off;
+    
+    % Capture and write frame
+    frm = getframe(gcf);
+    im = imresize(frame2im(frm),1);
+    writeVideo(vidfile,im)
+end
+close(vidfile)
+% set(gcf, 'position', [ 1   676   208*3   165*3]);
